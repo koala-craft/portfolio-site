34225d51c6bf7db1b126218b9fcd075d8756df7d
Kinomiya-Dai <utsunomiya.hiroki49@gmail.com>
2025-11-26 23:38:12 +0900

    ヘッダーナビゲーション対応途中

--- components/spNavButton.tsx
+++ components/spNavButton.tsx
@@ -16,22 +16,15 @@ const cx = (...classes) => classes.filter(Boolean).join(' ')
 
 export function Navbar() {
   return (
-    <nav
-      className={cx(
-        "flex flex-row justify-between items-center",
-        "relative md:relative",
-        "h-14 pr-4 pl-3 pb-0",
-        "sm:pr-12 sm:pl-10",
-        "border-b border-pf-ui-border dark:border-pf-ui-border-dark")}
-      id="nav"
-    >
-      <Link href="/">
-        <h1 className='px-2 font-bold leading-14'>Kinomiya Dai</h1>
-      </Link>
-      <div className={cx("flex flex-row items-center justify-end")}>
-        <div className={cx("hidden flex-row items-center justify-start space-x-0 mr-8",
-          "sm:flex"
-        )}>
+    <>
+      <button className=''></button>
+      {/* ナビゲーションエリア */}
+      <div>
+        <ul>
+          <Link href="/">
+            <li className='px-2 font-bold leading-14'>Home</li>
+          </Link>
+
           {Object.entries(navItems).map(([path, { name }]) => {
             return (
               <Link
@@ -40,14 +33,13 @@ export function Navbar() {
                 className={cx("text-underline-animate transition-all hover:opacity-75 flex align-middle relative py-1 px-2 m-1",
                 )}
               >
-                {name}
+                <li>{name}</li>
               </Link>
             )
           })}
-        </div>
-        <DarkModeToggle />
-        <button className='hidden fixed w-8 h-8' />
+        </ul>
+
       </div>
-    </nav >
+    </>
   )
 }
