dca5b59f0d44c90d10f748cdd56b843baeac833b
Kinomiya-Dai <utsunomiya.hiroki49@gmail.com>
2025-11-30 15:40:12 +0900

    next.jsの静的デプロイ設定の追加。 githubactionsの自動デプロイコードの追記

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['next-mdx-remote'],
+  trailingSlash: true,
+  output: 'export',
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
